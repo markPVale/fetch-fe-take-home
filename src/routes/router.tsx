70156d72136b1e510caa5f164aec67c9172fd1a2
Mark-Vale <markpvale@gmail.com>
2025-01-30 22:44:04 -0500

    Update routes with ProtectedRoute

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "../pages/loginPage";
-import SearchPage from "../pages/searchPage";
+import LoginPage from "../pages/LoginPage";
+import SearchPage from "../pages/SearchPage";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 const AppRoutes: React.FC = () => (
   <Router>
     <Routes>
       <Route path="/" element={<LoginPage />} />
-      <Route path="/search" element={<SearchPage />} />
+      <Route
+        path="/search"
+        element={
+          <ProtectedRoute>
+            <SearchPage />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   </Router>
 );
