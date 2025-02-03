f8219e302a4d7ef532ecf037643668c36cd29562
Mark-Vale <markpvale@gmail.com>
2025-02-02 21:29:41 -0500

    Update axios req

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,11 +3,12 @@ import axios from "axios";
 const fetchDogsApi = axios.create({
   baseURL: "https://frontend-take-home-service.fetch.com",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default fetchDogsApi;
 export * from "./auth";
 export * from "./dogs";
 export * from "./locations";
-
-
