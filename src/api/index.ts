47394e74e586ab6207c2805ab22b64cb4d50c601
Mark-Vale <markpvale@gmail.com>
2025-02-02 22:18:31 -0500

    Proxy request

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fetchDogsApi = axios.create({
-  baseURL: "https://frontend-take-home-service.fetch.com",
+  baseURL: "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
